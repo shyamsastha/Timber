8aba49ecdf66599b8a31f71270bdf1d9105c1a66
shyamsastha <34569410+shyamsastha@users.noreply.github.com>
2020-11-10 12:34:42 -0500

    Adding required files from the project and making small changes to test SFML basic framework.

--- Timber/Timber.cpp
+++ Timber/Timber.cpp
@@ -2,10 +2,19 @@
 //
 
 #include "pch.h"
+#include <SFML/Graphics.hpp>
 #include <iostream>
+
+// Make code easier to type with "using namespace"
+using namespace sf;
 
 int main()
 {
+	// Create a video mode object
+	VideoMode vm(1920, 1080);
+
+	// Create and open a window for the game
+	RenderWindow window(vm, "Timber!!", Style::Fullscreen);
 	return 0;
 }
 
